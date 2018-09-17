b2da1cb70c67d5c337e0cd71a30ba82caf6e0ef6
Jarek Kajko <jaruto7@gmail.com>
2018-09-17 19:16:18 +0200

    Use JS to format time system in modules directory

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,7 +1,5 @@
 var os = require( 'os' );
-var sec;
-var min;
-var hour;
+var createTime = require( '../modules/formatTime' );
 
 function getOSinfo() {
     var type = os.type();
@@ -11,36 +9,10 @@ function getOSinfo() {
         type = 'Windows';
     }
     var userInfo = os.userInfo();
-    var uptime = os.uptime();
     var release = os.release();
     console.log( 'User name: ', userInfo.username );
     console.log( 'Home dir: ', userInfo.homedir );
-    /*Stwórz moduł, który będzie poprawnie formatował czas. Zależy nam na napisaniu funkcji:
-    do przekształcania sekund na minuty - przykładowo, podając 125 sekund wyświetli 2 min. 5 sek.
-    sekund na godziny - podając 3700 sekund wyświetli 1 godz. 1 min. 40 sek.
-    dodaj moduł do katalogu modules i skorzystaj z niego w module OSinfo do poprawnego formatowania czasu działania systemu*/
-    
-    // console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    var uptimeFormat = ((uptime / 60).toFixed(0));
-    sec = 0; 
-    min = 0;
-    hour = 0;
-    if ( uptimeFormat > 0 ) {
-        hour++;
-        console.log( sec + ' sec' );
-        if( uptimeFormat > 60 ) {
-            min++;
-            console.log( min + ' min' );
-            sec++;
-            if( uptimeFormat > 3600 ) {
-                console.log( hour + ' hour' );
-            }
-        }
-    } else {
-        console.log( hour + ' hour ' + min + ' min ' + sec + ' sec' );
-    }
-            
-    // console.log( 'Uptime: ~', ( uptime / 1708674).toFixed( 0 ), 'min' + ' ' + ( uptime / 60).toFixed( 0 ), 'sec' );
+    createTime.print();
     console.log( 'System: ', type );
     console.log( 'Release: ', release );    
 }
---
+++ modules/formatTime.js
@@ -0,0 +1,18 @@
+var os = require( 'os' );
+var uptime = os.uptime();
+    
+    function formTime() {
+        var hours = uptime / 60;
+        var minutes = (uptime - (hours * 3600)) / 60;
+        var seconds = uptime - (hours * 3600) - (minutes * 60);
+    
+    if( hours > 0 ) {
+        console.log( 'Uptime: ~', ( hours ).toFixed( 0 ), 'hours ' + ( minutes ).toFixed( 0 ), 'min ' + ( seconds ).toFixed( 0 ), 'sec');
+    } else if( minutes > 0 ) {
+        console.log( 'Uptime: ~', ( minutes ).toFixed( 0 ), 'min ' + ( seconds ).toFixed( 0 ), 'sec');
+    } else if( seconds > 0 ) {
+        console.log( 'Uptime: ~', ( seconds ).toFixed( 0 ), 'sec');
+    }
+} 
+
+    exports.print = formTime;
