f0c168cb42f81e524e6682b9bc9dd3463c1b99e7
Jarek Kajko <jaruto7@gmail.com>
2018-09-28 21:31:12 +0200

    Fix format uptime code

--- modules/formatTime.js
+++ modules/formatTime.js
@@ -2,8 +2,8 @@ var os = require( 'os' );
 var uptime = os.uptime();
     
     function formTime() {
-        var hours = uptime / 60;
-        var minutes = (uptime - (hours * 3600)) / 60;
+        var hours = Math.floor(uptime / 3600);
+        var minutes = Math.floor((uptime - (hours * 3600)) / 60);
         var seconds = uptime - (hours * 3600) - (minutes * 60);
     
     if( hours > 0 ) {
